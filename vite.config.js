3f1c91cf92e77fd707273f156ec81d0720207892
CodeBaraa <barbara.hajduk12@gmail.com>
2026-04-24 11:40:00 +0200

    Some design changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: './', // Wichtig für Netlify Drop
+  base: '/goldenekruste/', // Wichtig für Netlify Drop
   build: {
     rollupOptions: {
       input: {
